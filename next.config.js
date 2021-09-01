0df413753dc17417f4afd3f48043937b53e2d508
emarzo <59425374+enzomarzo@users.noreply.github.com>
2021-09-01 10:43:19 -0300

    M3A07 - next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
+const redirectRules = require('./config/redirects');
+
 module.exports = {
   eslint: {
+    trailingSlash: true,
+    async redirects() {
+      return [
+        redirectRules,
+      ];
+    },
     ignoreDuringBuilds: true,
   },
 };
